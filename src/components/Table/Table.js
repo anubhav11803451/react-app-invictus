df3c4c43eecad051c83eaca73b92dfcfb0e76d8b
anubhav11803451 <anubhav.11803451@lpu.in>
2021-05-16 15:16:27 +0530

    ✨ added api call

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Table.css";
-import Form from "../Form/Form";
 
 function Table() {
-  return <div className="table">{/* <Form /> */}</div>;
+  const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const request = await getComputedStyle();
+  //   }
+  //   return request;
+  // }, [input]);
+
+  return (
+    <div className="table">
+      <table>
+        <tr>
+          <th className="words">Word</th>
+          <th className="count">Count</th>
+        </tr>
+        {/* {this.state.data.slice(0, this.state.number).map((item, index) => (
+          <tr>
+            <td className="words">{item.word}</td>
+            <td className="count">{item.count}</td>
+          </tr>
+        ))} */}
+      </table>
+    </div>
+  );
 }
 
 export default Table;
