d2ba519b9f1a71daedf420f60cb9eedb5dc605a1
anubhav11803451 <anubhav.11803451@lpu.in>
2021-05-17 01:57:32 +0530

    💄 Added Material UI

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,32 +1,61 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
 import "./Table.css";
 
-function Table({ wordCount, value }) {
+function Tables({ wordCount, value }) {
   // wordCount.map((item, index) => console.table(item));
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 500,
+    },
+    bold: {
+      fontWeight: "bolder",
+    },
+    styleTable: {
+      width: 500,
+      marginLeft: "auto",
+      marginRight: "auto",
+      borderRadius: 40,
+      overflowY: "auto",
+      scrollbarWidth: "none",
+    },
+  });
+
+  const classes = useStyles();
   return (
     <div className="table-container">
-      <table className="table">
-        <thead>
-          <tr>
-            <th className="word">
-              <h2>Words</h2>
-            </th>
-            <th className="coun">
-              <h2>Count</h2>
-            </th>
-          </tr>
-        </thead>
-        {wordCount.slice(0, value).map((item, index) => (
-          <tbody key={index}>
-            <tr>
-              <td className="words">{item.word}</td>
-              <td className="count">{item.count}</td>
-            </tr>
-          </tbody>
-        ))}
-      </table>
+      <TableContainer className={classes.styleTable} component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.bold} align="center">
+                Word
+              </TableCell>
+              <TableCell className={classes.bold} align="center">
+                Frequency
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {wordCount.slice(0, value).map((row, index) => (
+              <TableRow key={index}>
+                <TableCell align="center">{row.word}</TableCell>
+                <TableCell align="center">{row.count}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
 
-export default Table;
+export default Tables;
